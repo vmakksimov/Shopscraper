f158b6fa956716def32d4d84be9c3c2cb9a96eef
Viktor <vmakksimov@gmail.com>
2023-08-26 05:17:26 +0300

    adding crawl results

--- shopscraper/shopscraper/items.py
+++ shopscraper/shopscraper/items.py
@@ -6,7 +6,7 @@
 import scrapy
 
 
-class Product(scrapy.Item):
+class ProductItem(scrapy.Item):
     # define the fields for your item here like:
     name = scrapy.Field()
     price = scrapy.Field()
--- shopscraper/shopscraper/spiders/product.py
+++ shopscraper/shopscraper/spiders/product.py
@@ -2,7 +2,6 @@ import scrapy
 from selenium import webdriver
 from scrapy import Selector
 import time
-#from ..items import Product
 import json
 
 
@@ -10,43 +9,46 @@ class ProductSpider(scrapy.Spider):
     name = "product"
     allowed_domains = ["shop.mango.com"]
     start_urls = ["https://shop.mango.com/bg-en/men/t-shirts-plain/100-linen-slim-fit-t-shirt_47095923.html?c=07"]
-    # driver = webdriver.Chrome() - UNMARK TO WORK WITH SELENIUM
-
-    def start_requests(self):
-        url = "https://shop.mango.com/services/garments/068/en/S/4709592307"
-        request = scrapy.Request(url, callback=self.parse)
-        yield request
+    driver = webdriver.Chrome()
 
     def parse(self, response, *args, **kwargs):
-        raw_data = response.body
-        data = json.loads(raw_data)
-        name = data['name']
-        size = []
-        price = ''
-        colour = ''
-        for el in data['colors'].get('colors'):
-            price = el['price']['price']
-            colour = el['label']
-            [size.append(item['value']) for item in el['sizes']]
-            break
-
+        self.driver.get("".join(self.start_urls))
+        time.sleep(5)
+        sel = Selector(text=self.driver.page_source)
         yield {
-            'name' : name,
-            'price' : price,
-            'colour' : colour,
-            'size' : size
+            'name': "".join(sel.css('.product-name::text').extract()),
+            'price': float("".join(sel.css('.sAobE::text').extract()).split(' ')[1]),
+            'colour': "".join(sel.css('.colors-info-name::text').extract()),
+            'size': sel.css('.gk2V5::text').extract()
         }
 
-    #UNMARK TO RUN WITH SELENIUM
+    # UNMARK TO RUN WITH SCRAPY ONLY
 
+    # def start_requests(self):
+    #     url = "https://shop.mango.com/services/garments/068/en/S/4709592307"
+    #     request = scrapy.Request(url, callback=self.parse)
+    #     yield request
+    #
     # def parse(self, response, *args, **kwargs):
-    #     self.driver.get("".join(self.start_urls))
-    #     time.sleep(5)
-    #     sel = Selector(text=self.driver.page_source)
-    #     product = Product()
-    #     product['name'] = "".join(sel.css('.product-name::text').extract())
-    #     product['price'] = float("".join(sel.css('.sAobE::text').extract()).split(' ')[1])
-    #     product['colour'] = "".join(sel.css('.colors-info-name::text').extract())
-    #     product['size'] = sel.css('.gk2V5::text').extract()
-    #     yield product
+    #     raw_data = response.body
+    #     data = json.loads(raw_data)
+    #     name = data['name']
+    #     size = []
+    #     price = ''
+    #     colour = ''
+    #     for el in data['colors'].get('colors'):
+    #         price = el['price']['price']
+    #         colour = el['label']
+    #         [size.append(item['value']) for item in el['sizes']]
+    #         break
+    #
+    #     yield {
+    #         'name' : name,
+    #         'price' : price,
+    #         'colour' : colour,
+    #         'size' : size
+    #     }
+
+
+
 
